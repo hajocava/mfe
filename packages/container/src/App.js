7a8e882329ddafcec959c0230c84cb41924b7777
Haziel Castillo <haziel.castillo@truehome.com.mx>
2022-06-28 13:35:16 -0500

    Randomize production classNames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+})
 
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   )
 }
