1d606a5e8a3f3a373367c079666b6efd8b1dce1c
Haziel Castillo <haziel.castillo@truehome.com.mx>
2022-06-29 19:03:02 -0500

    Add memoryHistory and browserHistory

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,23 @@
 import { mount } from 'marketing/MarketingApp'
 import React, { useRef, useEffect } from 'react'
+import { useHistory } from 'react-router-dom';
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current)
-    })
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPath }) => {
+                const { pathname } = history.location;
+                if (pathname !== nextPath) {
+                    history.push(nextPath);
+                }
+            }
+        })
+
+        history.listen(onParentNavigate)
+    }, [])
 
     return <div ref={ref}></div>
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App'
 
 // Mount functions to start up the app
-const mount = elementHTML => ReactDOM.render(<App />, elementHTML);
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory()
+
+    if (onNavigate) {
+        history.listen(onNavigate)
+    }
+
+    ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate({ pathname: nextPath }) {
+            const { pathname } = history.location;
+            if (pathname !== nextPath) {
+                history.push(nextPath);
+            }
+        }
+    }
+}
 
 // If we are in development and in isolation, call mount immediately
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root')
 
-    if (devRoot) mount(devRoot)
+    if (devRoot) {
+        mount(devRoot, {
+            defaultHistory: createBrowserHistory()
+        })
+    }
 }
 
 // We are running through container and we should export the mount function
